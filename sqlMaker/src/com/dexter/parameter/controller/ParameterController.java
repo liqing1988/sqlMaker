67053495439120e3ece9a56c93b0d4f54d1e2f18
liqing1988 <liqing19881113@126.com>
2015-08-10 21:28:13 +0800

    第二次交叉更新

--- sqlMaker/src/com/dexter/parameter/controller/ParameterController.java
+++ sqlMaker/src/com/dexter/parameter/controller/ParameterController.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.dexter.conn.Conn;
 import com.dexter.parameter.entity.Parameter;
@@ -12,6 +14,7 @@ import com.dexter.parameter.entity.Parameter;
 public class ParameterController {
 	/*
 	 * 连接数据库获取参数列表
+	 * category: 查询的参数
 	 */
 	public List<Parameter> getParametersFromDBByCategory(String category){
 		List<Parameter> params = new ArrayList<Parameter>();
@@ -36,9 +39,32 @@ public class ParameterController {
 		return params;
 	}
 	
+	/*
+	 * 连接数据库获取参数列表
+	 * category: 查询的参数
+	 */
+	public Map<String, String> getMapFromDBByCategory(String category){
+		Map<String, String> map = new HashMap<String, String>();
+		try{
+			Connection conn = Conn.getConnectionForJDBC();
+			Statement statement = conn.createStatement();
+			String sql = "select name, value from cs_sys_parameter where category = '" + category + "'";
+					
+			ResultSet rs = statement.executeQuery(sql);
+			while(rs.next()){
+				//TODO 这样若是有相同名称的数据就会被覆盖。
+				map.put(rs.getString(1), rs.getString(2));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return map;
+	}
+	
 	public static void main(String[] args) {
 		ParameterController pc = new ParameterController();
-		pc.getParametersFromDBByCategory("education_degree");
+		//pc.getParametersFromDBByCategory("education_degree");
+		pc.getMapFromDBByCategory("education_degree");
 	}
 	
 }
--- sqlMaker/src/com/dexter/poi/ConvertExcel.java
+++ sqlMaker/src/com/dexter/poi/ConvertExcel.java
@@ -1,11 +1,13 @@
 package com.dexter.poi;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
@@ -13,10 +15,24 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
+import com.dexter.parameter.controller.ParameterController;
+
 public class ConvertExcel {
 
 	public void convertExcel(String filePath){
-		String[] paths = filePath.split("/");
+		//获取导出文件名
+		//转换参数并导出excel
+		
+		
+		
+	}
+	
+	/*
+	 * 获取导出文件的路径
+	 * filePath：源文件路径
+	 */
+	public String getOutFilePath(String inFilePath){
+		String[] paths = inFilePath.split("/");
 		String tempPath = "";	//转换后的excel文件路径，含路径
 		String tempFileName = "";	//转换后的excel名称
 		for(int i = 0; i < (paths.length - 1); i++){
@@ -27,9 +43,102 @@ public class ConvertExcel {
 		tempFileName += sdf.format(new Date()) + "_" + paths[paths.length-1];
 		
 		tempPath += tempFileName;
-		System.out.println(tempPath);
+		return tempPath;
+	}
+	
+	/*
+	 * 转换内容，导出excel
+	 * inFilePath: 源文件路径
+	 * params：替换参数，鉴于参数数量较少，可以将所有参数都放到一个Map中。
+	 */
+	public void exportExcel(String inFilePath, Map<Integer, Map<String, String>> params){
+		try {
+			HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(inFilePath));
+			//TODO 待确认是否写死sheet
+			HSSFSheet sheet = workbook.getSheet("居民");
+			HSSFRow row = null;
+			HSSFCell cell = null;
+			
+			//获取导出路径
+			String outFilePath = getOutFilePath(inFilePath);
+			FileOutputStream outStream = new FileOutputStream(outFilePath);
+			//循环读取源文件，替换参数
+			for(int i = 1; i<= sheet.getLastRowNum(); i++){
+				row = sheet.getRow(i);
+				for(int columnIndex : params.keySet()){
+					cell = row.getCell(columnIndex);
+					Map<String, String> parameter = params.get(columnIndex);
+					if(cell!=null){
+						if(parameter.containsKey(cell.getStringCellValue())){
+							cell.setCellValue(parameter.get(cell.getStringCellValue()));
+						}
+					}
+				}
+			}
+			
+			workbook.write(outStream);  
+			outStream.close();  	
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//输入excel
+		try {
+			HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(inFilePath));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
+	
+	/*
+	 * 转换内容，导出excel
+	 * inFilePath: 源文件路径
+	 * params：替换参数，使用不确定参数列表，可以在执行时手动添加。Integer表示字段列号， Map<String, String>表示参数值及显示名称
+	 */
+	/*public void exportExcel(String inFilePath, Map<Integer, Map<String, String>> ... params){
+		try {
+			HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(inFilePath));
+			//TODO 待确认是否写死sheet
+			HSSFSheet sheet = workbook.getSheet("");
+			HSSFRow row = null;
+			HSSFCell cell = null;
+			
+			//获取导出路径
+			String outFilePath = getOutFilePath(inFilePath);
+			FileOutputStream outStream = new FileOutputStream(outFilePath);
+			//循环读取源文件，替换参数; POI只能按行读取，无法按列读取，所以需要3次循环，后期有待提高处理速度
+			for(int i = 1; i<= sheet.getLastRowNum(); i++){
+				row = sheet.getRow(i);
+				for(Map<Integer, Map<String, String>> param : params){
+					for(int columnIndex : param.keySet()){
+						cell = row.getCell(columnIndex);
+						Map<String, String> parameter =param.get(columnIndex);
+						if(cell!=null){
+							if(parameter.containsKey(cell.getStringCellValue())){
+								cell.setCellValue(parameter.get(cell.getStringCellValue()));
+							}
+						}
+					}
+					
+				}
+				
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}*/
+	
 	
 	
 	public void replaceParameter(String inFileName, String sheetName, String outFileName, Map<Integer,Map<String, String>> parameters,String endarea) throws IOException{
@@ -94,6 +203,10 @@ public class ConvertExcel {
 	
 	public static void main(String[] args) {
 		ConvertExcel ce = new ConvertExcel();
-		ce.convertExcel("D:/tet/test.xls");
+		ParameterController pc = new ParameterController();
+		pc.getMapFromDBByCategory("education_degree");
+		Map<Integer, Map<String, String>> params = new HashMap<Integer, Map<String, String>>();
+		params.put(8, pc.getMapFromDBByCategory("relationship_to_householder"));
+		ce.exportExcel("E:/ExcelOut/龙家圈20150624.xls", params);
 	}
 }
